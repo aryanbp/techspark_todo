59721d4e1b37ad47d017ba9c08cfcd136654dd68
Beenu <aryanpanikar9@gmail.com>
2023-09-09 17:00:30 +0530

    Add Todo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import './App.css';
+import { useEffect, useRef, useState } from 'react';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const todoText = useRef();
 
+  function addTodo(event) {
+    event.preventDefault();
+    const next = [...todos, todoText.current.value];
+    setTodos(next);
+    localStorage.setItem('todos', JSON.stringify(next));
+    todoText.current.value = '';
+  }
+
+  return (
+    <div style={{ margin: '0 auto' }}>
+      <ul>
+        {todos.map((todo, index) => (<><li key={todo}>{todo}</li><button onClick={() => removeTodo(index)}>X</button></>))}
+      </ul><br />
+      <form onSubmit={addTodo}>
+        <input type='text' placeholder='What to do...' ref={todoText} /><br />
+        <input type='submit' value='Add' />
+      </form>
+    </div>
+  );
 }
 
 export default App;
