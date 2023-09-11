39d3bece6a53dd70d580492f155ea643a876a7b5
Beenu <aryanpanikar9@gmail.com>
2023-09-11 12:38:47 +0530

    Make Edit

--- src/TaskList.js
+++ src/TaskList.js
@@ -11,16 +11,25 @@ import IconButton from '@mui/joy/IconButton';
 import { Add, ExpandMoreOutlined } from '@mui/icons-material';
 import { Link } from 'react-router-dom';
 import Checkbox from '@mui/joy/Checkbox';
+import { Modal, ModalClose, Sheet, Typography } from '@mui/joy';
+
 
 function TaskList() {
     const [todos, setTodos] = useState([]);
     const [check, setCheck] = useState([]);
+    const [open, setOpen] = useState(false);
+    const dues = useRef([]);
+    const desc = useRef([]);
 
     useEffect(() => {
         const userTodos = localStorage.getItem('todos');
         const userCheck = localStorage.getItem('check');
+        const userDescription = localStorage.getItem('description');
+        const userDates = localStorage.getItem('dates');
         setCheck(userCheck ? JSON.parse(userCheck) : []);
         setTodos(userTodos ? JSON.parse(userTodos) : []);
+        dues.value = (userDates ? JSON.parse(userDates) : []);
+        desc.value = (userDescription ? JSON.parse(userDescription) : []);
     }, [])
 
     function removeTodo(index) {
@@ -28,11 +37,15 @@ function TaskList() {
         [...JSON.parse(localStorage.getItem('todos'))].splice(index, 1);
         check.splice(index, 1);
         [...JSON.parse(localStorage.getItem('check'))].splice(index, 1);
+        // (dues.value).splice(index, 1);
+        console.log(dues);
+        [...JSON.parse(localStorage.getItem('dates'))].splice(index, 1);
 
         setTodos([...todos])
         localStorage.setItem('todos', JSON.stringify([...todos]));
-        setCheck([...check])
+        setCheck([...check]);
         localStorage.setItem('check', JSON.stringify([...check]));
+        // localStorage.setItem('dates', JSON.stringify([...dues]));
     }
 
     return (
@@ -55,18 +68,61 @@ function TaskList() {
                     }}>Todo List{<IconButton><Link to='/taskview'><Add /></Link></IconButton>}</ListSubheader>
                     <List>
                         {todos.map((todo, index) => (
-                            <ListItem>
-                                <ListItemButton sx={{ gap: 2 }}>
-                                    <Checkbox color="primary" checked={check[index]} onChange={() => {
-                                        check[index] = (!check[index]);
-                                        setCheck([...check])
-                                        localStorage.setItem('check', JSON.stringify([...check]));
-                                    }} />
+                            <ListItem sx={{ gap: 2 }}>
+                                <Checkbox sx={{ marginTop: '6px' }} color="primary" checked={check[index]} onChange={() => {
+                                    check[index] = (!check[index]);
+                                    setCheck([...check])
+                                    localStorage.setItem('check', JSON.stringify([...check]));
+                                }} />
+                                <ListItemButton onClick={() => {
+                                    setOpen(true);
+                                    console.log(dues);
+                                }} sx={{ gap: 2 }}>
                                     {todo}
                                 </ListItemButton>
+                                <Modal
+                                    open={open}
+                                    onClose={() => setOpen(false)}
+                                    sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                                    <Sheet
+                                        variant="outlined"
+                                        sx={{
+                                            maxWidth: 500,
+                                            borderRadius: 'md',
+                                            p: 3,
+                                            boxShadow: 'lg',
+                                        }}
+                                    >
+                                        <ModalClose
+                                            variant="outlined"
+                                            sx={{
+                                                top: 'calc(-1/4 * var(--IconButton-size))',
+                                                right: 'calc(-1/4 * var(--IconButton-size))',
+                                                boxShadow: '0 2px 12px 0 rgba(0 0 0 / 0.2)',
+                                                borderRadius: '50%',
+                                                bgcolor: 'background.surface',
+                                            }}
+                                        />
+                                        <Typography
+                                            component="h2"
+                                            id="modal-title"
+                                            level="h4"
+                                            textColor="inherit"
+                                            fontWeight="lg"
+                                            mb={1}
+                                        >
+                                            Due at {(dues.value)[index]}
+                                        </Typography>
+                                        <Typography id="modal-desc" textColor="text.tertiary">
+                                            {(desc.value)[index]}
+                                        </Typography>
+                                    </Sheet>
+                                </Modal>
                                 <Box sx={{ display: 'flex', gap: 1 }}>
-                                    <Button size="sm" variant="plain" color="neutral">
-                                        Edit
+                                    <Button onClick={() => {
+                                        localStorage.setItem('index', index);
+                                    }} size="sm" variant="plain" color="neutral">
+                                        <Link to='/taskview'>Edit</Link>
                                     </Button>
                                     <Button onClick={() => removeTodo(index)} size="sm" variant="soft" color="danger">
                                         Delete
--- src/TaskView.js
+++ src/TaskView.js
@@ -1,43 +1,134 @@
-import { CssBaseline, CssVarsProvider } from '@mui/joy';
+import { DarkMode, LightMode } from '@mui/icons-material';
+import { Box, Button, CssBaseline, CssVarsProvider, FormControl, FormLabel, Input, List, ListItem, ListItemContent, Textarea, useColorScheme } from '@mui/joy';
 import { Container } from '@mui/material';
 import { useEffect, useRef, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
+
+function ModeToggle() {
+    const { mode, setMode } = useColorScheme();
+    const [mounted, setMounted] = useState(false);
+
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+    if (!mounted) {
+        return null;
+    }
+
+    return (
+        <Button
+            variant="soft"
+            onClick={() => {
+                setMode(mode === 'light' ? 'dark' : 'light');
+            }}
+        >
+            {mode === 'light' ? <DarkMode /> : <LightMode />}
+        </Button>
+    );
+}
 
 function TaskView() {
+    const navigate = useNavigate();
     const [todos, setTodos] = useState([]);
-    const todoText = useRef('');
-    const [check, setCheck] = useState([])
+    const [check, setCheck] = useState([]);
+    const [todoText, setTodoText] = useState('');
+    const [textArea, setTextArea] = useState('');
+    const [due, setDue] = useState('');
+    const [dates, setDates] = useState([]);
+    const [description, setDescription] = useState([]);
+    const [index, setIndex] = useState(-1);
 
     useEffect(() => {
         const userTodos = localStorage.getItem('todos');
         const userCheck = localStorage.getItem('check');
+        const editIndex = localStorage.getItem('index');
+        const userDescription = localStorage.getItem('description');
+        const userDates = localStorage.getItem('dates');
+        setIndex(editIndex ? JSON.parse(editIndex) : -1);
         setCheck(userCheck ? JSON.parse(userCheck) : []);
         setTodos(userTodos ? JSON.parse(userTodos) : []);
+        setDescription(userDescription ? JSON.parse(userDescription) : []);
+        setDates(userDates ? JSON.parse(userDates) : []);
     }, [])
 
+    useEffect(() => {
+        if (index >= 0) {
+            setTodoText(todos[index]);
+            setTextArea(description[index]);
+            setDue(dates[index]);
+            localStorage.setItem('index', -1);
+        }
+    }, [index])
+
     function addTodo(event) {
-        if (todoText.current.value != '') {
+        if (todoText != '' && index < 0) {
             event.preventDefault();
             // todos.unshift(todoText.current.value);
-            todos.push(todoText.current.value);
-            check.push(false)
+            todos.push(todoText);
+            check.push(false);
+            description.push(textArea);
+            dates.push(due);
             setTodos([...todos]);
+            setDescription([...description]);
+            setDates([...dates]);
             localStorage.setItem('todos', JSON.stringify([...todos]));
             localStorage.setItem('check', JSON.stringify([...check]));
-            todoText.current.value = '';
+            localStorage.setItem('description', JSON.stringify([...description]));
+            localStorage.setItem('dates', JSON.stringify([...dates]));
+            setTodoText('');
         }
+        else {
+            event.preventDefault();
+            todos[index] = todoText
+            description[index] = textArea
+            localStorage.setItem('todos', JSON.stringify([...todos]));
+            localStorage.setItem('description', JSON.stringify([...description]));
+            setTodoText('');
+        }
+        navigate(-1);
     }
 
     return (
         <Container>
             <CssVarsProvider>
                 <CssBaseline />
-                <form onSubmit={addTodo}>
-                    <input type='text' placeholder='What to do...' ref={todoText} /><br />
-                    <input type='submit' value='Add' />
-                </form>
+                <main>
+                    <ModeToggle />
+                    <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+
+                        <form onSubmit={addTodo} >
+                            <FormControl sx={{ gap: 2 }}>
+                                <FormLabel>Todo</FormLabel>
+                                <Input value={todoText} placeholder='Task to do...' onChange={(event) => { setTodoText(event.currentTarget.value) }} required />
+                            </FormControl>
+                            <FormControl sx={{ gap: 2 }}>
+                                <FormLabel>Description</FormLabel>
+                                <Textarea value={textArea} placeholder='Enter Description...' onChange={(event) => { setTextArea(event.currentTarget.value); }} />
+                            </FormControl>
+                            <FormControl sx={{ gap: 2 }}>
+                                <FormLabel>Due Date</FormLabel>
+                                <Input
+                                    value={due}
+                                    onChange={(event) => { setDue(event.currentTarget.value) }}
+                                    type="date"
+                                    slotProps={{
+                                        input: {
+                                            min: '2023-09-07T00:00',
+                                            max: '2030-09-14T00:00',
+                                        },
+                                    }}
+                                />
+                                <Button type='submit'>Add</Button>
+                                <Button onClick={() => {
+                                    navigate(-1);
+                                }}>Go Back</Button>
+                            </FormControl>
+                        </form>
+                    </Box>
+                </main>
             </CssVarsProvider>
-        </Container>
+        </Container >
     );
 }
 export default TaskView;
